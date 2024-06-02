7a8835373b485446c3bb15facad3acf43fc7c97c
Roman Pyshka <romasadova@gmail.com>
2024-06-02 21:50:25 +0300

    feat: connected database to server

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { User } from "./user/entities/user.entity";
+import { UserModule } from "./user/user.module";
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      port: 5437,
+      password: "toor",
+      username: "root",
+      entities: [User],
+      database: "nestdb",
+      synchronize: true,
+      logging: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
