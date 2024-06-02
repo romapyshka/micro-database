c31a723b7f2a9568772c6e3a12a7e51fd5f79302
romapyshka <33147170+romapyshka@users.noreply.github.com>
2024-06-03 02:40:49 +0300

    feat: deleted dist folder

--- dist/user/user.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { CreateUserDto } from "./dto/create-user.dto";
-import { User } from "./entities/user.entity";
-import { UserService } from "./user.service";
-export declare class UserController {
-    private readonly userService;
-    constructor(userService: UserService);
-    create(createUserDto: CreateUserDto): Promise<User>;
-}
--- dist/user/user.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { Repository } from "typeorm";
-import { CreateUserDto } from "./dto/create-user.dto";
-import { User } from "./entities/user.entity";
-export declare class UserService {
-    private readonly userRepository;
-    constructor(userRepository: Repository<User>);
-    createUser(createUserDto: CreateUserDto): Promise<User>;
-}
